7e42537492892c2a67551c6465eed309f333a6a7
heshan97 <heshanw2018@gmail.com>
2023-02-02 01:04:44 +0530

    HomeScreenadded to  app.js

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import { StatusBar } from "expo-status-bar"
 import { Box, NativeBaseProvider, Text } from "native-base";
-//import { Box,NativeBaseProvider, Text } from "native-base ";
 import HomeScreen from "./src/Screens/HomeScreen"
 
 export default function App() {
   return(
   <NativeBaseProvider>
-    <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
-      <HomeScreen/>
-    </Box>
+    <HomeScreen/>
   </NativeBaseProvider>
   );
   
