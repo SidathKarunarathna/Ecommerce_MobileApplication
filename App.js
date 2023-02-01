ed7d525bc9386d97fb6187c619a07796b0da663a
heshan97 <heshanw2018@gmail.com>
2023-02-02 00:56:25 +0530

    HomeScreen added to app.js

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar"
+import { Box, NativeBaseProvider, Text } from "native-base";
+//import { Box,NativeBaseProvider, Text } from "native-base ";
+import HomeScreen from "./src/Screens/HomeScreen"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Let's create our E-commerce Mobile Application!</Text>
-      <StatusBar style="auto" />
-    </View>
+  return(
+  <NativeBaseProvider>
+    <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+      <HomeScreen/>
+    </Box>
+  </NativeBaseProvider>
   );
+  
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
