bc70359825c6eb9dd9711278e9aa9c5555a133cd
SidathKarunarathna <Sidath21325@gmail.com>
2023-02-03 09:27:02 +0530

    Oder screen Cards added

--- src/Screens/PlaceOrderScreen.js
+++ src/Screens/PlaceOrderScreen.js
@@ -1,11 +1,44 @@
-import { Text, View } from "native-base";
+import { Box, ScrollView, Text, View } from "native-base";
 import React from "react";
+import Colors from "../color";
+import OrderInfo from "../Components/OrderInfo";
+import {FontAwesome,FontAwesome5,Ionicons} from '@expo/vector-icons'
 
 function PlaceOrderScreen() {
   return (
-    <View>
-      <Text>PlaceOrderScreen</Text>
-    </View>
+    <Box bg={Colors.subGreen} flex={1} safeArea pt={6}>
+      <Box>
+        <ScrollView horizontal={true} 
+        showsHorizontalScrollIndicator={false}>
+          <OrderInfo 
+          title="CUSTOMER" 
+          subTitle ="Admin Heshan" 
+          text="admin@gmail.com"
+          icon={<FontAwesome 
+          name="user" 
+          size={30} 
+          color={Colors.white}/>} />
+        
+        <OrderInfo 
+          title="SHIPPING INFO" 
+          subTitle ="Shipping: Kurunegala" 
+          text="Pay Method: Paypal"
+          icon={<FontAwesome5 
+          name="shipping-fast" size={30} 
+          color={Colors.white}/>} />
+        <OrderInfo 
+          title="DELIVER TO" 
+          subTitle ="Address: " 
+          text="Sidath Karunarathna, Bowaththa, Yakwila"
+          icon={<Ionicons
+          name="location-sharp" size={30} 
+          color={Colors.white}/>} />
+        </ScrollView>
+      </Box>
+      <Box px={6} flex={1} pb={3}>
+
+      </Box>
+    </Box>
   );
 }
 export default PlaceOrderScreen;
