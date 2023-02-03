19b850def862216e53b7ca270e339c661a189953
heshan97 <heshanw2018@gmail.com>
2023-02-03 17:38:36 +0530

    added label and input field to profile screen

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,10 +1,31 @@
-import { View, Text } from "react-native";
+import { Box, FormControl, Input, ScrollView, VStack } from "native-base";
 import React from "react";
+import Colors from "../../color";
 
 export default function Profile() {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <Box h="full" bg={Colors.white} px={5}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <VStack space={10} mt={5} pb={10}>
+          <FormControl>
+            <FormControl.Label
+              _text={{
+                fontSize: "12px",
+                fontWeight: "bold",
+              }}
+            >
+              USERNAME
+            </FormControl.Label>
+            <Input
+              borderWidth={0}
+              bg={Colors.subGreen}
+              py={3}
+              color={Colors.main}
+              fontSize={20}
+            />
+          </FormControl>
+        </VStack>
+      </ScrollView>
+    </Box>
   );
 }
