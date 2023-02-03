d89632a3a37d4d57a411979989924c31f45252f9
SidathKarunarathna <Sidath21325@gmail.com>
2023-02-03 11:40:32 +0530

    Order items added

--- src/Components/Profile/PlaceOrderModel.js
+++ src/Components/Profile/PlaceOrderModel.js
@@ -1,11 +1,27 @@
-import { View,Text } from "native-base"
-import React from "react"
+import { Center,Button  } from "native-base";
+import React,{useState} from "react"
+import Colors from "../../color";
+
 
 const PlaceOrderModel = () => {
+    const [showModel,setShowModel]=useState(false)
     return(
-        <View>
-            <Text>PlaceOrderModel</Text>
-        </View>
+        <Center>
+            <Button 
+            onPress={()=> setShowModel(true)} 
+            bg={Colors.black} 
+            color={Colors.white} 
+            mt={5}
+            rounded={30}
+            w="90%"
+            style={{
+                paddingTop:15,
+                paddingBottom:15
+            }}
+            >
+                SHOW TOTAL
+                </Button>
+        </Center>
     );
 };
 export default PlaceOrderModel
