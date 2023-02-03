f28677cb2dea29cb309a6104619ef2fa12eab439
kchamithri <gangodaw-im18025@stu.kln.ac.lk>
2023-02-03 12:46:28 +0530

    Single Product view UI

--- src/Screens/SingleProductScreen.js
+++ src/Screens/SingleProductScreen.js
@@ -1,11 +1,14 @@
-import { Text, View } from "native-base";
+import { Box, Image, ScrollView, Text, View } from "native-base";
 import React from "react";
+import Colors from "../color";
 
 function SingleProductScreen() {
   return (
-    <View>
-      <Text>SingleProductScreen</Text>
-    </View>
+    <Box safeArea flex={1} bg={Colors.white}>
+      <ScrollView px={5} showsVerticalScrollIndicator={false}>
+        {/* <Image source={require("..")} */}
+      </ScrollView>
+    </Box>
   );
 }
 
