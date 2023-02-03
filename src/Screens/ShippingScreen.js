6b4ce32a6014e9327c543fcf728e45cc5dc1cb00
PavithMadushan <pavithmadushan@gmail.com>
2023-02-04 00:54:15 +0530

    shippingscreen started

--- src/Screens/ShippingScreen.js
+++ src/Screens/ShippingScreen.js
@@ -1,11 +1,16 @@
-import { Text, View } from "native-base";
 import React from "react";
+import { Box,Center,Text} from "native-base";
+import Colors from "../color";
 
 function ShippingScreen() {
   return (
-    <View>
-      <Text>ShippingScreen</Text>
-    </View>
+    <Box flex={1} safeArea bg={Colors.main} py={5}>
+      <Center pb={15}>
+        <Text color={Colors.white} fontSize={14} bold>
+          
+        </Text>
+      </Center>
+    </Box>
   );
 }
 export default ShippingScreen;
