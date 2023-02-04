fd528111cd1d9b55ac85cac5e86f7b02ac514d73
SidathKarunarathna <sidath21325@gmail.com>
2023-02-04 12:46:01 +0530

    Order Screen Added

--- App.js
+++ App.js
@@ -4,12 +4,13 @@ import { Box, NativeBaseProvider, Text } from "native-base";
 // import HomeScreen from "./src/Screens/HomeScreen";
 // import LoginScreen from "./src/Screens/LoginScreen";
 // import PlaceOrderScreen from "./src/Screens/PlaceOrderScreen";
-import ProfileScreen from "./src/Screens/ProfileScreen";
+// import ProfileScreen from "./src/Screens/ProfileScreen";
+import OrderScreen from "./src/Screens/OrderScreen";
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <PlaceOrderScreen />
+      <OrderScreen />
     </NativeBaseProvider>
   );
 }
--- src/Screens/OrderScreen.js
+++ src/Screens/OrderScreen.js
@@ -1,11 +1,47 @@
-import { Text, View } from "native-base";
+import { Box, Heading, ScrollView, Text, View } from "native-base";
 import React from "react";
+import Colors from "../color";
+import OrderInfo from "../Components/Profile/OrderInfo";
+import {FontAwesome5,Ionicons} from '@expo/vector-icons'
+import OrderItem from "../Components/Profile/OrderItems";
+import OrderModel from "../Components/Profile/OrderModel";
 
 function OrderScreen() {
   return (
-    <View>
-      <Text>OrderScreen</Text>
-    </View>
+    <Box bg={Colors.subGreen} flex={1} safeArea pt={6}>
+      <Box>
+        <ScrollView horizontal={true} 
+        showsHorizontalScrollIndicator={false}>
+        <OrderInfo 
+          title="SHIPPING INFO" 
+          success
+          subTitle ="Shipping: Kurunegala" 
+          text="Pay Method: Paypal"
+          icon={<FontAwesome5 
+          name="shipping-fast" size={30} 
+          color={Colors.white}/>} />
+        <OrderInfo 
+          title="DELIVER TO" 
+          danger
+          subTitle ="Address: " 
+          text="Sidath Karunarathna, Bowaththa, Yakwila"
+          icon={<Ionicons
+          name="location-sharp" size={30} 
+          color={Colors.white}/>} />
+        </ScrollView>
+      </Box>
+      <Box px={6} flex={1} pb={3}>
+            <Heading 
+            bold
+            fontSize={15} 
+            isTruncated
+            my={4}>
+              PRODUCTS
+            </Heading>
+            <OrderItem/>
+            <OrderModel/>
+      </Box>
+    </Box>
   );
 }
 export default OrderScreen;
