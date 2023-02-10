884d9e828e9e4c610c55eae1f7dfa115b79d77f5
PavithMadushan <pavithmadushan@gmail.com>
2023-02-10 11:17:17 +0530

    Cart.js model class added

---
+++ server/models/Cart.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const CartSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  products: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+});
+
+export default mongoose.model("Cart", CartSchema);
