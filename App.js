9d214bd5e76e499c3c21705f99c2014f15246e11
heshan97 <heshanw2018@gmail.com>
2023-02-11 15:14:37 +0530

    create reviewController.js file

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import { Box, NativeBaseProvider, Text } from "native-base";
-// import SingleProductScreen from "./src/Screens/SingleProductScreen";
-// import HomeScreen from "./src/Screens/HomeScreen";
-// import LoginScreen from "./src/Screens/LoginScreen";
-// import PlaceOrderScreen from "./src/Screens/PlaceOrderScreen";
+import SingleProductScreen from "./src/Screens/SingleProductScreen";
+//import HomeScreen from "./src/Screens/HomeScreen";
+import LoginScreen from "./src/Screens/LoginScreen";
+import PlaceOrderScreen from "./src/Screens/PlaceOrderScreen";
 import ProfileScreen from "./src/Screens/ProfileScreen";
 
 export default function App() {
