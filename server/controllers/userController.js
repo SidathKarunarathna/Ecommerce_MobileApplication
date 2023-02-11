68e47b03f117b21873b4c884a626f935fed15fcb
kchamithri <gangodaw-im18025@stu.kln.ac.lk>
2023-02-11 15:53:56 +0530

    login added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
-import user from "../models/user.js";
+import bcryptjs from "bcryptjs";
+import User from "../models/User.js";
 
 export const signUp = async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
-    const createUser = await user.create({
+    const createUser = await User.create({
       name,
       email,
       password,
@@ -13,7 +14,6 @@ export const signUp = async (req, res) => {
 
     res.status(200).json({
       message: "success",
-      user: createUser,
     });
   } catch (error) {
     console.log(error);
@@ -21,5 +21,35 @@ export const signUp = async (req, res) => {
       message: "Error creating new user",
       error,
     });
+  }
+};
+
+export const signIn = async (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const loggedUser = await User.findOne({ email: email });
+
+    if (loggedUser) {
+      const isMatch = await bcryptjs.compare(password, loggedUser.password);
+
+      if (isMatch) {
+        res.status(200).json({
+          message: "Success",
+          user: {
+            id: loggedUser._id,
+            name: loggedUser.name,
+            email: loggedUser.email,
+          },
+        });
+      } else {
+        res.status(400).json({
+          message: "Invalid Credentials",
+        });
+      }
+    }
+  } catch (error) {
+    res.status(400).send(error);
   }
 };
