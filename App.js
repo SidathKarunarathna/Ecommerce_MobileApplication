d5f0909833e9258f5879ef6bc60ac85f80d4a7c8
SidathKarunarathna <Sidath21325@gmail.com>
2023-02-11 20:51:58 +0530

    Register screen added with backend

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <NativeBaseProvider>
       <NavigationContainer>
-        {/* <StatusBar hidden={true}/> */}
+         <StatusBar translucent={true}  backgroundColor={'transparent'}/> 
         <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown:false}}>
           <Stack.Screen name="Login" component={LoginScreen}/>
           <Stack.Screen name="Register" component={RegisterScreen}/>
